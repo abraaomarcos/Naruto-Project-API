0dd66cebd382a87628f39d8ddca154de690170e9
abraaomarcos <abraao.m3@gmail.com>
2023-05-29 09:56:33 -0300

    adicionei atributos à classe poder

--- Naruto.Api/Naruto.Api/Models/Poder.cs
+++ Naruto.Api/Naruto.Api/Models/Poder.cs
@@ -2,5 +2,10 @@
 {
     public class Poder
     {
+        public int Agua { get; set; }
+        public int Fogo { get; set; }
+        public int Raio { get; set; }
+        public int Terra { get; set; }
+        public int Vento { get; set; }
     }
 }
